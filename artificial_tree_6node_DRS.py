88418f051ca562d0e2d6ee792b7b3c6c9bbd85e6
Arnob Alam <arnoblalam@gmail.com>
2016-02-03 19:09:28 -0500

    add artificial_tree from Huancheng

---
+++ artificial_tree_6node_DRS.py
@@ -0,0 +1,44 @@
+import tree
+
+artificial_tree = {
+1:set([2,3,4,5]),
+2:set([]),
+3:set([6,7]),
+4:set([8,9]),
+5:set([10,11,12]),
+6:set([]),
+7:set([]),
+8:set([]),
+9:set([]),
+10:set([]),
+11:set([]),
+12:set([])
+}
+
+artificial_tree_weights ={
+1: 100,
+2: 20,
+3: 50,
+4: 200,
+5: 10,
+6: 30,
+7: 20,
+8: 30,
+9: 150,
+10:5,
+11:10,
+12:13
+}
+
+#import math
+
+#def myAggregationFunction(x, y):
+#    alpha = (2**((x+y)/10-5))**-1
+#    return (x+y)**alpha
+    
+#results = tree.aggregate(artificial_tree, artificial_tree_weights, 6, 70000, sort_type="maximum", f=myAggregationFunction)
+
+
+results = tree.aggregate(artificial_tree, artificial_tree_weights, 6, 10, sort_type="maximum", f=lambda x,y: x+y)
+
+#tree.draw_tree(artificial_tree)
